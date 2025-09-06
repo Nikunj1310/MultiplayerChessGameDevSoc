10cf156c3610975c22a068596061ec2be4b79744
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2025-09-07 04:43:54 +0530

    added lobby functions like join or create room

--- Assets/Scripts/CreateAndJoin.cs
+++ Assets/Scripts/CreateAndJoin.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class CreateAndJoin : MonoBehaviour
+public class CreateAndJoin : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField inputCreate;
+    [SerializeField] private TMP_InputField inputJoin;
+
+    public void CreateRoom()
     {
-        
+        PhotonNetwork.CreateRoom(inputCreate.text);
+        // TODO: cover players being able to create room with empty name
+        // TODO: cover players being in same room but on different photon servers
+        // TODO: cover Photon.Realtime, RoomOptions, TypedLobby, string[] expectedUsers
+    }
+    // TODO: Have a automated room ID generated for user to create room easier
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(inputJoin.text);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnJoinedRoom()
     {
-        
+        Debug.Log("Joined room successfully");
+        PhotonNetwork.LoadLevel("Scenes/Game");
     }
+
+    // TODO: Add a button for quick join, which joins a random room
+
+    // TODO: Add error handling for room creation/joining failures, have a canvas that displays errors, and add a button what user can click to turn back to lobby, or close the canvas automatically after a few seconds
 }
