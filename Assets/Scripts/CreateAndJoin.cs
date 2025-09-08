e778e418ed6c2d1bbf0695d255fadcc63cadf8b6
Nikunj Goyal <nikunjgoyal1106@gmail.com>
2025-09-09 03:27:53 +0530

    added room connectivity

--- Assets/Scripts/CreateAndJoin.cs
+++ Assets/Scripts/CreateAndJoin.cs
@@ -4,20 +4,51 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class CreateAndJoin : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private TMP_InputField inputCreate;
+    [SerializeField] private TextMeshProUGUI inputCreate;
     [SerializeField] private TMP_InputField inputJoin;
+    [SerializeField]
+    RoomOptions roomOptions = new RoomOptions()
+    {
+        MaxPlayers = 2,
+        IsVisible = false,
+        IsOpen = true,
+        PlayerTtl = 60000, // 1 minute
+        EmptyRoomTtl = 60000 // 1 minute
+    };
+    [SerializeField] private TextMeshProUGUI roomJoinOrCreateErrorMessage;
+    void Start()
+    {
+        inputCreate.text = GenerateRandomRoomName();
+    }
+    //  automated room ID generated for user to create room easier
+    public void GenerateRandomRoomID()
+    {
+        inputCreate.text = GenerateRandomRoomName();
+    }
 
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(inputCreate.text);
-        // TODO: cover players being able to create room with empty name
-        // TODO: cover players being in same room but on different photon servers
-        // TODO: cover Photon.Realtime, RoomOptions, TypedLobby, string[] expectedUsers
+
+
+        PhotonNetwork.CreateRoom(inputCreate.text, roomOptions);
+        Debug.Log("Creating room: " + inputCreate.text);
+
     }
-    // TODO: Have a automated room ID generated for user to create room easier
+
+    private string GenerateRandomRoomName(int length = 8)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        System.Text.StringBuilder result = new System.Text.StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            result.Append(chars[Random.Range(0, chars.Length)]);
+        }
+        return "Room_" + result.ToString();
+    }
 
     public void JoinRoom()
     {
@@ -31,6 +62,22 @@ public class CreateAndJoin : MonoBehaviourPunCallbacks
     }
 
     // TODO: Add a button for quick join, which joins a random room
+    public void QuickJoinRoom()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom(null, 2, MatchmakingMode.FillRoom, null, null, null, roomOptions);
+    }
 
     // TODO: Add error handling for room creation/joining failures, have a canvas that displays errors, and add a button what user can click to turn back to lobby, or close the canvas automatically after a few seconds
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError("Room creation failed: " + message);
+        roomJoinOrCreateErrorMessage.text = "Room creation failed: " + message;
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError("Room joining failed: " + message);
+        roomJoinOrCreateErrorMessage.text = "Room joining failed: " + message;
+    }
+
 }
